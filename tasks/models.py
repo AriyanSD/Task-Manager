78fd7a6001750805ca34553b9ed39a74922a38e4
Ariyan Saadatmand <ariyan.saadatmand@gmail.com>
2025-11-25 16:51:47 +0330

    bug fix

--- tasks/models.py
+++ tasks/models.py
@@ -16,7 +16,7 @@ class Task(models.Model):
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True,null=False)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_TODO)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
